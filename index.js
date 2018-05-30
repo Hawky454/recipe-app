36fd2bb5821d9294ade4595d8af060f9e58371ec
David Miller <hawky454@gmail.com>
2018-05-30 11:52:28 -0500

    added a working api

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ let searchButton = document.getElementById('searchButton');
 let searchField = document.getElementById('searchField');
 
 searchButton.addEventListener('click', function(event) {
-  event.preventDefault();
-  if(searchField.value === '') {
-    alert('Please type a recipe in the search input');
-    searchField.setAttribute('style', 'background-color: #ff000073');
-  } else {
-    console.log('this is where I need to put an api fetch.');
-  }
-
-})
+      event.preventDefault();
+      if (searchField.value === '') {
+        alert('Please type a recipe in the search input');
+        searchField.setAttribute('style', 'background-color: #ff000073');
+      } else {
+        fetch(`https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?q=${searchField.value}`)
+          .then(response => response.json())
+          .then(data => console.log(data));
+      }
+    });
