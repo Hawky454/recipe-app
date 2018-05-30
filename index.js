0eed290ad6a48095d8bcc1a03a0ae7a55e5aef8a
David Miller <hawky454@gmail.com>
2018-05-30 08:37:46 -0500

    added some basic javascript

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('testing 123');
+let searchButton = document.getElementById('searchButton');
+let searchField = document.getElementById('searchField');
+
+searchButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  if(searchField.value === '') {
+    alert('Please type a recipe in the search input');
+    searchField.setAttribute('style', 'background-color: #ff000073');
+  } else {
+    console.log('this is where I need to put an api fetch.');
+  }
+
+})
